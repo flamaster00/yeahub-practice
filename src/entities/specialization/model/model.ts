284991435b98a478df2e4a049bb3752f79ed28af
Alexander <name.way@gmail.com>
2026-04-29 17:05:09 +0300

    add specializationApi

--- src/entities/specialization/model/model.ts
+++ src/entities/specialization/model/model.ts
@@ -1,8 +1,20 @@
 export interface Specialization {
-    id: number;
-    title: string;
-    description: string;
-    imageSrc: any;
-    createdAt: string;
-    updatedAt: string;
-  }
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  imageSrc: string;
+  createdAt: string;
+  updatedAt: string;
+  createdBy: {
+    id: string;
+    username: string;
+  };
+}
+
+export interface GetAllSpecializationsResponse {
+  total: number;
+  page: number;
+  limit: number;
+  data: Specialization[];
+}
