1801b2339ec9fcec71767f30b427aea78fcabf26
Alexander <name.way@gmail.com>
2026-06-16 23:15:17 +0300

    add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "@app/router";
+import { Provider } from "react-redux";
+import { store } from "@app/store";
 
 export const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />;
+    </Provider>
+  );
 };
